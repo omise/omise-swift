b2fd0b3b38aa3fcdd350803116198c0d9f0a4e7e
Chakrit Wichian <service@chakrit.net>
2016-04-20 14:17:02 +0700

    Initial project creation.

---
+++ OmiseSwift/OmiseSwift.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OmiseSwift.
+FOUNDATION_EXPORT double OmiseSwiftVersionNumber;
+
+//! Project version string for OmiseSwift.
+FOUNDATION_EXPORT const unsigned char OmiseSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmiseSwift/PublicHeader.h>
+
+
