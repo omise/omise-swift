9fe0b3fc5ad1d9007eb483f5e5e4c808076969ce
Chakrit Wichian <chakrit@users.noreply.github.com>
2016-06-28 18:38:52 +0700

    Dispute API. (#12)

--- OmiseSwift/Dispute.swift
+++ OmiseSwift/Dispute.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public class Dispute: ResourceObject {
+    public override class var resourcePath: String { return "/disputes" }
+    
     public var amount: Int64? {
         get { return get("amount", Int64Converter.self) }
         set { set("amount", Int64Converter.self, toValue: newValue) }
@@ -24,5 +26,43 @@ public class Dispute: ResourceObject {
     public var charge: String? {
         get { return get("charge", StringConverter.self) }
         set { set("charge", StringConverter.self, toValue: newValue) }
+    }
+}
+
+public class DisputeParams: Params {
+    public var message: String? {
+        get { return get("message", StringConverter.self) }
+        set { set("message", StringConverter.self, toValue: newValue) }
+    }
+}
+
+extension Dispute: Listable { }
+extension Dispute: InstanceRetrievable { }
+
+extension Dispute: Updatable {
+    public typealias UpdateParams = DisputeParams
+}
+
+extension Dispute {
+    public static func list(using given: Client? = nil, state: DisputeStatus, params: ListParams? = nil, callback: Request<Dispute.ListOperation>.Callback) -> Request<Dispute.ListOperation>? {
+        let attributes = params?.normalizedAttributes ?? [:]
+        let operation = ListOperation(klass: self, attributes: attributes)
+        operation.path += state.rawValue
+        
+        let client = resolveClient(given: given)
+        return client.call(operation, callback: callback)
+    }
+}
+
+func exampleDispute() {
+    let _: [DisputeStatus] = [.Open, .Pending, .Closed] // valid statuses
+
+    Dispute.list(state: .Open) { (result) in
+        switch result {
+        case let .Success(list):
+            print("open disputes: \(list.data.count)")
+        case let .Fail(err):
+            print("error: \(err)")
+        }
     }
 }
