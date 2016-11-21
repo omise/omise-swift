cbbd7239b273db4d98621fd91d0d76637237d612
Anak Mirasing <thaihooligan@hotmail.com>
2016-11-21 10:07:28 +0700

    Adopting Swift 3 to currency (#24)
    * Adopting Swift 3 to currency and add number of digits properties
    
    * Change convert from/to subunit function name
    
    * Change jyp to jpy case
    
    * Add symbol properties and change numberOfDigits to numberOfFractionDidigits
    
    * Remove numberOfFractionDigits
    
    * Remove numberOfFractionDigits from custom init
    
    * Remove symbol properties from currency

--- OmiseSwift/Currency.swift
+++ OmiseSwift/Currency.swift
@@ -1,55 +1,52 @@
 import Foundation
 
-
 public let centBasedCurrencyFactor = 100
 public let identicalBasedCurrencyFactor = 1
 
 public enum Currency {
-  case THB
-  case JPY
-  case Custom(code: String, factor: Int)
-  
-  
-  public var code: String {
-    switch self {
-    case .THB:
-      return "THB"
-    case .JPY:
-      return "JPY"
-    case.Custom(code: let code, factor: _):
-      return code
+    case thb
+    case jpy
+    case custom(code: String, factor: Int)
+    
+    
+    public var code: String {
+        switch self {
+        case .thb:
+            return "THB"
+        case .jpy:
+            return "JPY"
+        case.custom(code: let code, factor: _):
+            return code
+        }
     }
-  }
-  
-  /// A convertion factor represents how much Omise amount equals to 1 unit of this currency. eg. THB's factor is equals to 100.
-  public var factor: Int {
-    switch self {
-    case .THB:
-      return centBasedCurrencyFactor
-    case .JPY:
-      return identicalBasedCurrencyFactor
-    case .Custom(code: _, factor: let factor):
-      return factor
+    
+    /// A convertion factor represents how much Omise amount equals to 1 unit of this currency. eg. THB's factor is equals to 100.
+    public var factor: Int {
+        switch self {
+        case .thb:
+            return centBasedCurrencyFactor
+        case .jpy:
+            return identicalBasedCurrencyFactor
+        case .custom(code: _, factor: let factor):
+            return factor
+        }
     }
-  }
-  
-  public func convertFromSubunit(value: Int64) -> Double {
-    return Double(value) / Double(factor)
-  }
-  
-  public func convertToSubunit(value: Double) -> Int64 {
-    return Int64(value * Double(factor))
-  }
-  
-  public init?(code: String) {
-    switch code.uppercased() {
-    case "THB":
-      self = .THB
-    case "JPY":
-      self = .JPY
-    default: return nil
+    
+    public func convert(fromSubunit value: Int64) -> Double {
+        return Double(value) / Double(factor)
     }
-  }
+    
+    public func convert(toSubunit value: Double) -> Int64 {
+        return Int64(value * Double(factor))
+    }
+    
+    public init?(code: String) {
+        switch code.uppercased() {
+        case "THB":
+            self = .thb
+        case "JPY":
+            self = .jpy
+        default: return nil
+        }
+    }
 }
-
-
