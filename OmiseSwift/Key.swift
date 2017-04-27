ca10eba8974504833a337ea6ef7a8b7f80023375
Pitiphong Phongpattranont <pitiphong.p@me.com>
2017-04-27 15:55:01 +0700

    Added: Key data type for a better key handling
    This commit introduces a new data type `Key` and its `KeyKind` companion type. The Key instance will hold a key information for the client to communicate with the backend.
    
    We have 2 `KeyKind` at the moment,
    1. Application Key: this key type is used when communicating with the `API` backend.
    2. Public Key: this key type is used when communicating with the `Valut` backend.
    Those 2 KeyKind are declared as empty enums and be used as a phantom type on `Key`

---
+++ OmiseSwift/Key.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public protocol KeyKind {
+    static var prefix: String { get }
+    static var kind: String { get }
+}
+
+public enum ApplicationKey: KeyKind {
+    public static let prefix: String = "akey"
+    public static let kind: String = "application"
+}
+
+public enum PublicKey: KeyKind {
+    public static let prefix: String = "pkey"
+    public static let kind: String = "public"
+}
+
+
+public struct Key<Kind: KeyKind>: OmiseIdentifiableObject, OmiseLiveModeObject {
+    public let id: String
+    public let object: String
+    public let isLive: Bool
+    public let createdDate: Date
+    
+    public let key: String
+    public let name: String?
+    
+    public init?(JSON json: Any) {
+        guard let json = json as? [String: Any],
+            let omiseProperties = Key.parseOmiseProperties(JSON: json),
+            let key = json["key"] as? String,
+            let kind = json["kind"] as? String,
+            Kind.kind == kind, key.hasPrefix(Kind.prefix) else {
+                return nil
+                
+        }
+        
+        (self.object, self.isLive, self.id, self.createdDate) = omiseProperties
+        self.key = key
+        self.name = json["name"] as? String
+    }
+}
