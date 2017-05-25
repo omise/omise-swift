77343e9d3d94dcdd44362d75a678a70083385a84
Pitiphong Phongpattranont <pitiphong.p@me.com>
2017-05-25 14:57:39 +0700

    Make Occurrence.Status conforms to `Equatable` protocol

--- OmiseSwift/Occurrence.swift
+++ OmiseSwift/Occurrence.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 
-public struct Occurrence<Data: Schedulable>: OmiseResourceObject {
+public struct Occurrence<Data: Schedulable>: OmiseLocatableObject, OmiseIdentifiableObject {
     
     public enum Status {
         case skipped(String)
@@ -17,14 +17,12 @@ public struct Occurrence<Data: Schedulable>: OmiseResourceObject {
     public let location: String
     
     public let id: String
-    public let isLive: Bool
     public let createdDate: Date
     
-    
     public let schedule: DetailProperty<Schedule<Data>>
-    public let scheduleDate: Date
+    public let scheduleDate: DateComponents
     
-    public let retryDate: Date
+    public let retryDate: DateComponents?
     public let processedDate: Date
     
     public let status: Status
@@ -36,17 +34,16 @@ public struct Occurrence<Data: Schedulable>: OmiseResourceObject {
 extension Occurrence {
     public init?(JSON json: Any) {
         guard let json = json as? [String: Any],
-            let omiseObjectProperties = Occurrence<Data>.parseOmiseResource(JSON: json) else {
+            let omiseObjectProperties = Occurrence<Data>.parseOmiseProperties(JSON: json) else {
                 return nil
         }
         
-        (self.object, self.location, self.id, self.isLive, self.createdDate) = omiseObjectProperties
+        (self.object, self.location, self.id, self.createdDate) = omiseObjectProperties
         
         guard
             let status = Occurrence.Status(JSON: json),
             let schedule = json["schedule"].flatMap(DetailProperty<Schedule<Data>>.init(JSON:)),
-            let scheduleDate = json["schedule_date"].flatMap(DateConverter.convert(fromAttribute:)),
-            let retryDate = json["schedule_date"].flatMap(DateConverter.convert(fromAttribute:)),
+            let scheduleDate = json["schedule_date"].flatMap(DateComponentsConverter.convert(fromAttribute:)),
             let processedDate = json["processed_at"].flatMap(DateConverter.convert(fromAttribute:)),
             let result = json["result"].flatMap(DetailProperty<Data>.init(JSON:)) else {
                 return nil
@@ -55,14 +52,35 @@ extension Occurrence {
         self.status = status
         self.schedule = schedule
         self.scheduleDate = scheduleDate
-        self.retryDate = retryDate
         self.processedDate = processedDate
         self.result = result
+        self.retryDate = json["retry_date"].flatMap(DateComponentsConverter.convert(fromAttribute:))
     }
 }
 
 
-extension Occurrence.Status {
+extension Occurrence.Status: Equatable {
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func ==(lhs: Occurrence<Data>.Status, rhs: Occurrence<Data>.Status) -> Bool {
+        switch (lhs, rhs) {
+        case (.successful, .successful):
+            return true
+        case (.failed(let lhsMessage), .failed(let rhsMessage)):
+            return lhsMessage == rhsMessage
+        case (.skipped(let lhsMessage), .skipped(let rhsMessage)):
+            return lhsMessage == rhsMessage
+        default:
+            return false
+        }
+    }
+
     init?(JSON json: Any) {
         guard let json = json as? [String: Any],
             let status = json["status"] as? String else {
