43ee36ae056bc629a9ed015e3f0820fe6be415ea
Pitiphong Phongpattranont <pitiphong.p@me.com>
2017-05-26 18:03:47 +0700

    Update some test cases to adopt the new design

--- OmiseSwiftTests/URLEncoderTest.swift
+++ OmiseSwiftTests/URLEncoderTest.swift
@@ -1,11 +1,16 @@
 import Foundation
-import Omise
+@testable import Omise
 import XCTest
+
+
+struct AnyJSONAttributes: APIDataSerializable {
+    let json: JSONAttributes
+}
 
 class URLEncoderTest: OmiseTestCase {
     func testEncodeBasic() {
         let values: JSONAttributes = ["hello": "world"]
-        let result = URLEncoder.encode(values)
+        let result = encode(AnyJSONAttributes(json: values))
         XCTAssertEqual("hello", result[0].name)
         XCTAssertEqual("world", result[0].value)
     }
@@ -21,7 +26,7 @@ class URLEncoderTest: OmiseTestCase {
             "6date": Date(timeIntervalSince1970: 0)
         ]
         
-        let result = URLEncoder.encode(values).map({ (query) in query.value ?? "(nil)" })
+        let result = encode(AnyJSONAttributes(json: values)).map({ (query) in query.value ?? "(nil)" })
         XCTAssertEqual(result, [
             "world",
             "42",
@@ -29,7 +34,7 @@ class URLEncoderTest: OmiseTestCase {
             "1234123412341234",
             "false",
             "true",
-            "1970-01-01T07:00:00+07:00"
+            "1970-01-01T00:00:00Z"
             ])
     }
     
@@ -40,7 +45,7 @@ class URLEncoderTest: OmiseTestCase {
             "2deeper": ["nesting": ["also": "works"]  ] 
         ]
         
-        let result = URLEncoder.encode(values)
+        let result = encode(AnyJSONAttributes(json: values))
         XCTAssertEqual("0outer", result[0].name)
         XCTAssertEqual("normal", result[0].value)
         XCTAssertEqual("1nested[inside]", result[1].name)
@@ -50,15 +55,15 @@ class URLEncoderTest: OmiseTestCase {
     }
     
     func testConverter() {
-        let searchFilterParams = ChargeFilterParams()
+        var searchFilterParams = ChargeFilterParams()
         searchFilterParams.amount = 1000
-        searchFilterParams.cardLastDigits = "4242"
-        searchFilterParams.capture = true
-        searchFilterParams.created = DateComponents(year: 2016, month: 8, day: 1)
+        searchFilterParams.cardLastDigits = LastDigits(lastDigitsString: "4242")!
+        searchFilterParams.isCaptured = true
+        searchFilterParams.created = DateComponents(calendar: Calendar(identifier: .gregorian), year: 2016, month: 8, day: 1)
         
-        let result = Set(URLEncoder.encode(searchFilterParams.normalizedAttributes).map({ (query) in query.value ?? "(nil)" }))
+        let result = Set(encode(searchFilterParams).map({ (query) in query.value ?? "(nil)" }))
         XCTAssertEqual(result, [
-            "1000",
+            "1000.0",
             "4242",
             "true",
             "2016-8-1"
