464c764bb365e8cf62a2305f52204a9717adfbb1
Pitiphong Phongpattranont <pitiphong.p@me.com>
2017-09-27 19:24:01 +0700

    Implement card data encoding

--- OmiseSwift/Card.swift
+++ OmiseSwift/Card.swift
@@ -225,11 +225,28 @@ extension TokenizedCard {
     }
     
     public func encode(to encoder: Encoder) throws {
-        fatalError()
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(object, forKey: .object)
+        try container.encode(id, forKey: .id)
+        try container.encode(isLive, forKey: .isLive)
+        try container.encode(createdDate, forKey: .createdDate)
+        try container.encode(lastDigits, forKey: .lastDigits)
+        try container.encode(brand, forKey: .brand)
+        try container.encode(name, forKey: .name)
+        try container.encodeIfPresent(bankName, forKey: .bankName)
+        try container.encodeIfPresent(postalCode, forKey: .postalCode)
+        try container.encodeIfPresent(countryCode, forKey: .countryCode)
+        try container.encodeIfPresent(city, forKey: .city)
+        try container.encodeIfPresent(financing, forKey: .financing)
+        try container.encode(fingerPrint, forKey: .fingerPrint)
+        try container.encodeIfPresent(expiration?.month, forKey: .expirationMonth)
+        try container.encodeIfPresent(expiration?.year, forKey: .expirationYear)
     }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        
         object = try container.decode(String.self, forKey: .object)
         id = try container.decode(String.self, forKey: .id)
         isLive = try container.decode(Bool.self, forKey: .isLive)
@@ -301,7 +318,24 @@ extension CustomerCard {
     }
     
     public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
         
+        try container.encode(object, forKey: .object)
+        try container.encode(location, forKey: .location)
+        try container.encode(id, forKey: .id)
+        try container.encode(isLive, forKey: .isLive)
+        try container.encode(createdDate, forKey: .createdDate)
+        try container.encode(lastDigits, forKey: .lastDigits)
+        try container.encode(brand, forKey: .brand)
+        try container.encode(name, forKey: .name)
+        try container.encodeIfPresent(bankName, forKey: .bankName)
+        try container.encodeIfPresent(postalCode, forKey: .postalCode)
+        try container.encodeIfPresent(countryCode, forKey: .countryCode)
+        try container.encodeIfPresent(city, forKey: .city)
+        try container.encodeIfPresent(financing, forKey: .financing)
+        try container.encode(fingerPrint, forKey: .fingerPrint)
+        try container.encodeIfPresent(expiration?.month, forKey: .expirationMonth)
+        try container.encodeIfPresent(expiration?.year, forKey: .expirationYear)
     }
     
     public init(from decoder: Decoder) throws {
