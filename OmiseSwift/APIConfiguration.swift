59af72ec53c74768f7bff6c522e7d96d2b4c3d25
Pitiphong Phongpattranont <pitiphong.p@me.com>
2017-10-10 15:19:52 +0700

    Update the `API Version` to be `2017-09-05`

--- OmiseSwift/APIConfiguration.swift
+++ OmiseSwift/APIConfiguration.swift
@@ -6,7 +6,7 @@ public protocol AccessKey {
 
 
 public struct APIConfiguration {
-    public let apiVersion: String = "2015-11-17"
+    public let apiVersion: String = "2017-09-05"
     public let accessKey: AccessKey
     
     public init(key: AccessKey) {
