d6b22a54576f021ae04c321ea929c5b701c7caca
Pitiphong Phongpattranont <pitiphong.p@me.com>
2018-02-05 15:11:46 +0700

    Implement missing Collection conformance

--- OmiseSwift/ListProperty.swift
+++ OmiseSwift/ListProperty.swift
@@ -12,20 +12,16 @@ public struct ListProperty<Item: OmiseObject>: OmiseObject {
     public var offset: Int
     
     public var data: [Item]
-    
-    public subscript(index: Array<Item>.Index) -> Item {
-        return data[index]
-    }
-    
-    public subscript(bounds: Range<Array<Item>.Index>) -> ArraySlice<Item> {
-        return data[bounds]
-    }
 }
 
 extension ListProperty: RandomAccessCollection {
-    /// The position of the first element in a nonempty collection.
-    ///
-    /// If the collection is empty, `startIndex` is equal to `endIndex`.
+    public subscript(position: (Array<Item>.Index)) -> Item {
+        return data[position]
+    }
+    public subscript(bounds: Range<Array<Item>.Index>) -> ArraySlice<Item> {
+        return data[bounds]
+    }
+    
     public var startIndex: (Array<Item>.Index) {
         return data.startIndex
     }
@@ -33,6 +29,17 @@ extension ListProperty: RandomAccessCollection {
     public var endIndex: (Array<Item>.Index) {
         return data.endIndex
     }
-}
 
+    public func index(before i: Array<Item>.Index) -> Array<Item>.Index {
+        return data.index(before: i)
+    }
+    
+    public func index(after i: (Array<Item>.Index)) -> (Array<Item>.Index) {
+        return data.index(after: i)
+    }
+    
+    public func index(_ i: (Array<Item>.Index), offsetBy n: Int) -> (Array<Item>.Index) {
+        return data.index(i, offsetBy: n)
+    }
+}
 
