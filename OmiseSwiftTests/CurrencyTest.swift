25445504f70f14eb7951e99209ede2a26132680a
Pitiphong Phongpattranont <pitiphong.p@me.com>
2019-07-10 13:25:32 +0700

    Added newly supported currencies information (#80)

--- OmiseSwiftTests/CurrencyTest.swift
+++ OmiseSwiftTests/CurrencyTest.swift
@@ -7,43 +7,98 @@ class CurrencyTest: XCTestCase {
     func testSupportedCurrencies() {
         do {
             let currency = Currency(code: "THB")
+            XCTAssertEqual(currency, Currency.thb)
             XCTAssertEqual(currency?.code, "THB")
             XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
             let currency = Currency(code: "JPY")
+            XCTAssertEqual(currency, Currency.jpy)
             XCTAssertEqual(currency?.code, "JPY")
             XCTAssertEqual(currency?.factor, 1)
         }
         
         do {
             let currency = Currency(code: "IDR")
+            XCTAssertEqual(currency, Currency.idr)
             XCTAssertEqual(currency?.code, "IDR")
             XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
             let currency = Currency(code: "SGD")
+            XCTAssertEqual(currency, Currency.sgd)
             XCTAssertEqual(currency?.code, "SGD")
             XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
             let currency = Currency(code: "USD")
+            XCTAssertEqual(currency, Currency.usd)
             XCTAssertEqual(currency?.code, "USD")
             XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
             let currency = Currency(code: "GBP")
+            XCTAssertEqual(currency, Currency.gbp)
             XCTAssertEqual(currency?.code, "GBP")
             XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
             let currency = Currency(code: "EUR")
+            XCTAssertEqual(currency, Currency.eur)
             XCTAssertEqual(currency?.code, "EUR")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "MYR")
+            XCTAssertEqual(currency, Currency.myr)
+            XCTAssertEqual(currency?.code, "MYR")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "AUD")
+            XCTAssertEqual(currency, Currency.aud)
+            XCTAssertEqual(currency?.code, "AUD")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "CAD")
+            XCTAssertEqual(currency, Currency.cad)
+            XCTAssertEqual(currency?.code, "CAD")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "CHF")
+            XCTAssertEqual(currency, Currency.chf)
+            XCTAssertEqual(currency?.code, "CHF")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "CNY")
+            XCTAssertEqual(currency, Currency.cny)
+            XCTAssertEqual(currency?.code, "CNY")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+        
+        do {
+            let currency = Currency(code: "DKK")
+            XCTAssertEqual(currency, Currency.dkk)
+            XCTAssertEqual(currency?.code, "DKK")
+            XCTAssertEqual(currency?.factor, 100)
+        }
+
+        do {
+            let currency = Currency(code: "HKD")
+            XCTAssertEqual(currency?.code, "HKD")
             XCTAssertEqual(currency?.factor, 100)
         }
     }
@@ -53,36 +108,6 @@ class CurrencyTest: XCTestCase {
             let currency = Currency(code: "KRW")
             XCTAssertEqual(currency?.code, "KRW")
             XCTAssertEqual(currency?.factor, 1)
-        }
-        
-        do {
-            let currency = Currency(code: "MYR")
-            XCTAssertEqual(currency?.code, "MYR")
-            XCTAssertEqual(currency?.factor, 100)
-        }
-        
-        do {
-            let currency = Currency(code: "CNY")
-            XCTAssertEqual(currency?.code, "CNY")
-            XCTAssertEqual(currency?.factor, 100)
-        }
-        
-        do {
-            let currency = Currency(code: "AUD")
-            XCTAssertEqual(currency?.code, "AUD")
-            XCTAssertEqual(currency?.factor, 100)
-        }
-        
-        do {
-            let currency = Currency(code: "CAD")
-            XCTAssertEqual(currency?.code, "CAD")
-            XCTAssertEqual(currency?.factor, 100)
-        }
-        
-        do {
-            let currency = Currency(code: "HKD")
-            XCTAssertEqual(currency?.code, "HKD")
-            XCTAssertEqual(currency?.factor, 100)
         }
         
         do {
