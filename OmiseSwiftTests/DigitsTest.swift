da3f571163a9fce234be3b77abaa5e953d0bc8eb
Pitiphong Phongpattranont <pitiphong.p@me.com>
2019-08-06 13:02:48 +0700

    Merge the First and Last Digits Tests into DigitTests

---
+++ OmiseSwiftTests/DigitsTest.swift
@@ -0,0 +1,56 @@
+import XCTest
+import Omise
+
+
+class DigitsTest: XCTestCase {
+    func testCreateDigitsSuccessfully() {
+        let firstDigits = Digits(digitsString: "1234")
+        XCTAssertNotNil(firstDigits)
+        XCTAssertEqual(firstDigits?.digits, "1234")
+        
+        let secondDigits = Digits(digitsString: "1111")
+        XCTAssertNotNil(secondDigits)
+        XCTAssertEqual(secondDigits?.digits, "1111")
+        
+        let thirdDigits = Digits(digitsString: "4242")
+        XCTAssertNotNil(thirdDigits)
+        XCTAssertEqual(thirdDigits?.digits, "4242")
+        
+        let forthDigits = Digits(digitsString: "123")
+        XCTAssertNotNil(forthDigits)
+        XCTAssertEqual(forthDigits?.digits, "123")
+        
+        let fifthDigits = Digits(digitsString: "12345")
+        XCTAssertNotNil(fifthDigits)
+        XCTAssertEqual(fifthDigits?.digits, "12345")
+        
+        let sixthDigits = Digits(digitsString: "123456")
+        XCTAssertNotNil(sixthDigits)
+        XCTAssertEqual(sixthDigits?.digits, "123456")
+        
+        let seventhDigits = Digits(digitsString: "42")
+        XCTAssertNotNil(seventhDigits)
+        XCTAssertEqual(seventhDigits?.digits, "42")
+
+    }
+    
+    func testFailyCreateDigits() {
+        let firstTry = Digits(digitsString: "")
+        XCTAssertNil(firstTry)
+        
+        let secondTry = Digits(digitsString: "abcd")
+        XCTAssertNil(secondTry)
+        
+        let thirdTry = Digits(digitsString: "123a")
+        XCTAssertNil(thirdTry)
+    }
+    
+    func testDigitsEquality() {
+        let firstDigits = Digits(digitsString: "1234")!
+        let secondDigits = Digits(digitsString: "1234")!
+        let thirdDigits = Digits(digitsString: "4242")!
+        
+        XCTAssertEqual(firstDigits, secondDigits)
+        XCTAssertNotEqual(firstDigits, thirdDigits)
+    }
+}
