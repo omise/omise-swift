29134f7beddc868a05c64698b4c9491c08f725c1
Tobias Hafermalz <tobias@omise.co>
2020-07-01 13:13:14 +0700

    Added additional tests for WeChatPay

--- OmiseSwiftTests/SourceOperationFixtureTests.swift
+++ OmiseSwiftTests/SourceOperationFixtureTests.swift
@@ -461,6 +461,61 @@ class SourceOperationFixtureTests: FixtureTestCase {
                        """)
     }
     
+    func testCreateBarcodeWeChatPaySource() throws {
+        let expectation = self.expectation(description: "Creating Barcode WeChatPay Source")
+        
+        let weChatPayBarcode = WeChatPayBarcodeParams(barcode: "1234567890")
+        let createParams = PaymentSourceParams(amount: 1000000, currency: .thb,
+                                               type: .barcode(Barcode.weChatPay(weChatPayBarcode)))
+        
+        _ = PaymentSource.create(using: testClient, params: createParams, callback: { result in
+            defer { expectation.fulfill() }
+            
+            switch result {
+            case let .success(source):
+                XCTAssertEqual(source.amount, 1000000)
+                XCTAssertEqual(source.currency, .thb)
+                XCTAssertEqual(source.sourceType, .barcode(SourceType.Barcode.weChatPay))
+                XCTAssertEqual(source.flow, Flow.offline)
+                if case .barcode(.weChatPay(let weChatPayBarcodeResult)) = source.paymentInformation {
+                    XCTAssertEqual(weChatPayBarcodeResult.barcode, "1234567890123456")
+                } else {
+                    XCTFail("Wrong payment information")
+                }
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        })
+        
+        waitForExpectations(timeout: 15.0, handler: nil)
+    }
+    
+    func testEncodeWeChatPayBarcodeSource() throws {
+        let weChatPayBarcode = WeChatPayBarcodeParams(barcode: "1234567890123456")
+        let createSourceParams = PaymentSourceParams(amount: 1_000, currency: .thb,
+                                                     type: .barcode(.weChatPay(weChatPayBarcode)))
+        
+        let encoder = URLQueryItemEncoder()
+        encoder.arrayIndexEncodingStrategy = .emptySquareBrackets
+        
+        let createSourceEncodedString = String(data: URLQueryItemEncoder.encodeToFormURLEncodedData(
+            queryItems: try encoder.encode(createSourceParams)), encoding: .utf8)
+        XCTAssertEqual(createSourceEncodedString,
+                       """
+                       amount=1000&currency=THB&type=barcode_wechat&barcode=1234567890123456
+                       """)
+        
+        let createChargeParams = ChargeParams(value: Value.init(amount: 10_000_00, currency: .thb),
+                                              sourceType: .barcode(Barcode.weChatPay(weChatPayBarcode)))
+        let createChargeEncodedString = String(data: URLQueryItemEncoder.encodeToFormURLEncodedData(
+            queryItems: try encoder.encode(createChargeParams)), encoding: .utf8)
+        XCTAssertEqual(createChargeEncodedString,
+                       """
+                       amount=1000000&currency=THB&source%5Btype%5D=barcode_wechat&\
+                       source%5Bbarcode%5D=1234567890123456
+                       """)
+    }
+    
     func testInstallmentBAYSourceRetrieve() {
         let expectation = self.expectation(description: "Installment KBank Source result")
         
