e115dd9f37ccd3e85104d81597c2fb4a98779041
Donut <ponpol@omise.co>
2021-03-02 12:44:18 +0700

    remove coding keys encode decode for bankstatus

--- OmiseSwift/API Models/BankStatus.swift
+++ OmiseSwift/API Models/BankStatus.swift
@@ -12,27 +12,4 @@ public struct BankStatus: Codable, Hashable {
    public let code: String
    public let name: String
    public let active: Bool
-
-   private enum CodingKeys: String, CodingKey {
-        case code
-        case name
-        case active
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        name = try container.decode(String.self, forKey: .name)
-        code = try container.decode(String.self, forKey: .code)
-        active = try container.decode(Bool.self, forKey: .active)
-
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
-        try container.encode(name, forKey: .name)
-        try container.encode(code, forKey: .code)
-        try container.encode(active, forKey: .active)
-    }
 }
