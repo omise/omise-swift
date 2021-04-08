b602d5798e3ae7311ea4fc49db0d7c6570f11a4c
Tobias <tobias@omise.co>
2021-04-08 09:22:12 +0700

    Add support for SPM (#110)
    * Add support for spm
    
    * Fix trailing_comma

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Omise",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Omise",
+            type: .dynamic,
+            targets: ["Omise"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Omise",
+            path: "OmiseSwift",
+            exclude: ["Info-iOS.plist", "Info-OSX.plist", "Omise.h"],
+            resources: [.process("Resources")]
+        )
+    ]
+)
